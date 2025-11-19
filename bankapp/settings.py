b1d67ba07e7389c02e239d2b5d2aabab2a6bc690
gabinvesteasy <gabinvesteasy@gmail.com>
2025-11-19 17:47:16 +0300

    Fix ALLOWED_HOSTS for Render deployment

--- bankapp/settings.py
+++ bankapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-)u=z+=(n#v*6o5qy_pcq&%b))q*i!&ux8o=z6j845nxy=z8qnz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['website-cikq.onrender.com', '.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
